49fea7505927dc4b0231fad00aedd12d3cf9ef2b
Vasyl Kucherak <vasil.kucherak@gmail.com>
2022-06-08 17:45:28 +0300

    v.0.2.5(added login function on server)

--- server/auth.router.js
+++ server/auth.router.js
@@ -1,8 +1,10 @@
 import Router from 'express';
 import bcrypt from 'bcryptjs';
 import { check, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
 import User from './user.model.js';
+import Key from './key.js';
 
 const authRouter = new Router();
 
@@ -21,10 +23,38 @@ authRouter.post('/registration',
             if (candidate) {
                 return res.status(400).json({message: `User with email ${email} alredy exist.`});
             }
-            const hashPassword = bcrypt.hashSync(password, 15);
+            const hashPassword = bcrypt.hashSync(password, 7);
             const user = new User({email, password: hashPassword});
             await user.save();
             return res.json({message: "User was created!"});
+    } catch (e) {
+        console.log(e);
+        res.send({message: "Server error"})
+    }
+});
+
+authRouter.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body;
+            const user = await User.findOne({email});
+            if (!user) {
+                return res.status(400).json({message: "User not found"});
+            }
+            const validPassword = bcrypt.compareSync(password, user.password);
+            if (!validPassword) {
+                return res.status(400).json({message: "Invaild password"});
+            }
+            const token = jwt.sign({id: user.id}, Key.secretKey, {expiresIn: "1h"});
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            });
     } catch (e) {
         console.log(e);
         res.send({message: "Server error"})
